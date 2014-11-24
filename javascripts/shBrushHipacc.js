31e048decf9cf5652783323149d8c8b24ea21aa1
Richard Membarth <richard.membarth@uni-saarland.de>
2014-11-24 11:45:50 +0100

    Update keywords.

--- javascripts/shBrushHipacc.js
+++ javascripts/shBrushHipacc.js
@@ -7,7 +7,7 @@ SyntaxHighlighter.brushes.Hipacc = function()
   var datatypes = 'uchar4 ushort4 uint4 char4 short4 int4 float4 double4';
 
   var keywords = 'Image Pyramid Accessor Kernel IterationSpace Mask Domain ' +
-      'BoundaryCondition output convolve reduce iterate traverse ' +
+      'BoundaryCondition output output_at pixel_at convolve reduce iterate traverse ' +
       'Boundary::MIRROR Boundary::UNDEFINED Boundary::REPEAT Boundary::CONSTANT ' +
       'Interpolate::NO Interpolate::NN Interpolate::LF Interpolate::CF Interpolate::L3 ' +
       'Reduce::SUM Reduce::MIN Reduce::MAX Reduce::PROD Reduce::MEDIAN';
